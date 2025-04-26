3d6f0c52ae94c5e019d552bd5d7c1ab1ed075e54
Dmitry Olegovich Sorokin (@sorydima) <43252008+sorydima@users.noreply.github.com>
2025-04-26 13:14:15 +0300

    diff ..

--- qml/pages/AboutPage.qml
+++ qml/pages/AboutPage.qml
@@ -35,7 +35,7 @@ Page {
                 font.pixelSize: Theme.fontSizeSmall
                 textFormat: Text.RichText
                 wrapMode: Text.WordWrap
-                text: qsTr("<p>The project provides an example of using WebView on the CEF engine.</p>")
+                text: qsTr("<p>Welcome to Marina.Moda where we share information related to Music, Streaming, Blockchain, WEB3. We're dedicated to providing you the very best information and knowledge of the above mentioned topics! We hope you found all of the information on Marina.Moda helpful, as we love to share them with you. If you require any more information or have any questions about our site, please feel free to contact us by email at support@rechain.network.</p>")
             }
 
             SectionHeader {
--- translations/ru.auroraos.ChromiumBrowser.ts
+++ translations/ru.auroraos.ChromiumBrowser.ts
@@ -10,7 +10,7 @@
     </message>
     <message>
         <location filename="../qml/pages/AboutPage.qml" line="38"/>
-        <source>&lt;p&gt;The project provides an example of using WebView on the CEF engine.&lt;/p&gt;</source>
+        <source>&lt;p&gt;Welcome to Marina.Moda where we share information related to Music, Streaming, Blockchain, WEB3. We're dedicated to providing you the very best information and knowledge of the above mentioned topics! We hope you found all of the information on Marina.Moda helpful, as we love to share them with you. If you require any more information or have any questions about our site, please feel free to contact us by email at support@rechain.network.&lt;/p&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
